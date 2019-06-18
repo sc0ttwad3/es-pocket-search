ca7058b36a92ad65501b2d676565a415c649f651
Scott Wade <scottwade.net@live.com>
2019-06-17 20:09:48 -0400

    Alter console message display

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config();
 const fastify = require('fastify')({ logger: false })
 
 const port = process.env.PORT || 3000;
+const mode = process.env.NODE_ENV || "unspecified";
 
 // Declare a route
 fastify.get('/', async (request, reply) => {
@@ -14,7 +15,8 @@ const start = async () => {
   try {
     await fastify.listen(port);
     //fastify.log.info(`server listening on ${fastify.server.address().port}`);
-    console.log(chalk.blueBright(`[ ✓ ] server listening on ${fastify.server.address().port}`));
+    console.log(chalk.green(`[ ✓ ] server in ${mode} mode listening on ${fastify.server.address().port}`));
+    console.log(chalk.greenBright('[Press CTRL-C to stop]'));
   } catch (err) {
     // fastify.log.error(err);
     console.log(chalk.redBright(`[ ✗ ] ${err}`));
