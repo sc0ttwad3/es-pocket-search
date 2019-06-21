aeceaeb3138bf940b28aa9258c060d59885c65bb
Scott Wade <scottwade.net@live.com>
2019-06-21 14:46:37 -0400

    Add section to retrieve tokens with node-getpocket

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk');
 const dotenv = require('dotenv').config();
+const { dd, du } = require('dumper');
 const fastify = require('fastify')({ logger: false });
 const GetPocket = require('node-getpocket');
 
@@ -10,6 +11,8 @@ const mode = process.env.NODE_ENV || "unspecified";
  * OAuth 2.0 Authentication
  *
  * Using: node-getpocket module
+ *
+ * Will eventually use AXIOS directly.
  */
 
 const POCKET_CONSUMER_KEY = process.env.POCKET_CONSUMER_KEY;
@@ -50,10 +53,18 @@ fastify.route({
   method: 'GET',
   url: '/login',
   handler: async (request, reply) => {
+
     const pocket = new GetPocket({
       consumer_key: POCKET_CONSUMER_KEY,
       redirect_uri: POCKET_REDIRECT_URI
     });
+
+    //
+    //
+    // MAYBE replace all this with AXIOS directly
+    // in a external function?
+    //
+    //
 
     pocket.getRequestToken({ redirect_uri: POCKET_REDIRECT_URI}, (err, resp, body) => {
       if (err) {
@@ -63,7 +74,21 @@ fastify.route({
         POCKET_REQUEST_TOKEN = json.code;
         console.log(`Request_Token:  ${POCKET_REQUEST_TOKEN}`);
       }
+    });
+
+    pocket.getAuthorizeURL({ consumer_key: POCKET_CONSUMER_KEY, request_token: POCKET_REQUEST_TOKEN});
+
+    pocket.getAccessToken({ request_token: POCKET_REQUEST_TOKEN}, (err, resp, body) => {
+      if (err) {
+        console.log(chalk.redBright('[ X ] ERROR gettomg AccessToken...\n', err));
+      } else {
+        const json = JSON.parse(body)
+        POCKET_ACCESS_TOKEN = json.access_token;
+        console.log(`Access_Token:  ${POCKET_ACCESS_TOKEN}`);
+      }
     })
+
+
     return { this_is: 'login' }
   }
 });
